15f29c7af13c373b9d6aad5af8408d673698c38e
Fran Parra <franparra2@gmail.com>
2020-11-28 19:39:21 +0100

    script altavoces

---
+++ script musical_harrypotter.ps1
@@ -0,0 +1,115 @@
+﻿#Función Beep 
+#Beep(Frecuencia,Duración)
+ 
+#Freq: la frecuencia del sonido, en hertz
+#Duración: duración del sonido, en milisegundos
+ 
+#Frecuencias notas 4ª octava
+#Do4	261,625565 Hz
+#Re4	293,664768 Hz
+#Mi4	329,627557 Hz
+#Fa4	349,228231 Hz
+#Sol4	391,995436 Hz
+#La4	440,000000 Hz
+#Si4	493,883301 Hz
+ 
+
+
+#primera estrofa
+[System.Console]::Beep(329,500)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(440,500)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(261,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(493,500)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(440,500)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(329,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(293,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(493,500)
+Start-Sleep -milliseconds 200
+#segunda estrofa
+[System.Console]::Beep(440,500)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(261,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(493,500)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(391,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(440,500)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(329,500)
+Start-Sleep -milliseconds 200
+
+#tercera estrofa
+[System.Console]::Beep(329,500)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(349,500)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(261,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(493,500)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(391,500)
+Start-Sleep -milliseconds 200
+#cuarta estrofa
+[System.Console]::Beep(329,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(391,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(349,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(349,1000)
+Start-Sleep -milliseconds 200
+#quinta estrofa
+[System.Console]::Beep(293,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(329,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(261,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(440,500)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(329,500)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(261,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(440,500)
+Start-Sleep -milliseconds 200
+#sexta estrofa
+[System.Console]::Beep(261,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(329,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(261,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(329,1000)
+Start-Sleep -milliseconds 200
+#septima estrofa
+ [System.Console]::Beep(261,1000)
+ Start-Sleep -milliseconds 200
+ [System.Console]::Beep(391,1000)
+ Start-Sleep -milliseconds 200
+ [System.Console]::Beep(349,1000)
+ Start-Sleep -milliseconds 200
+ [System.Console]::Beep(349,1000)
+ Start-Sleep -milliseconds 200
+ #octava estrofa
+ [System.Console]::Beep(293,1000)
+ Start-Sleep -milliseconds 200
+[System.Console]::Beep(329,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(261,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(440,500)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(329,500)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(261,1000)
+Start-Sleep -milliseconds 200
+[System.Console]::Beep(440,500)
